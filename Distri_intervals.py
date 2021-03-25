30646c7ee1da15dc83548b6a8716e3111a033599
SaraOukkal <sara.oukkal@ens-lyon.fr>
2021-03-25 09:37:24 +0100

    modification du script Distri_intervals.py

--- Distri_intervals.py
+++ Distri_intervals.py
@@ -27,7 +27,7 @@ def distri (intervals):
 
 def plot (distribution, output):
 	
-	plt.hist(distribution, bins=20, color=salmon)
+	plt.hist(distribution, bins=20, color="salmon")
 	plt.title("Intervals size distribution")
 	#plt.xticks(np.arange(50))
 	plt.xlabel("Size (in nt)")
