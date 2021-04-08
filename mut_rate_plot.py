ce027907537b8fbd23178c35aee0a3585b0fe441
SaraOukkal <sara.oukkal@ens-lyon.fr>
2021-04-08 13:56:29 +0200

    pimp plots

--- mut_rate_plot.py
+++ mut_rate_plot.py
@@ -22,10 +22,14 @@ def mut_rate_plot(mut_rate, output):
 	for l in mut_R:
 		if not l.startswith("d"): #Ignore le header du fichier
 			line_MR=l.strip().split("\t")
-			dist.append(float(line_MR[0])) #Charge les données des mutations dans les variables associées
-			MR.append(float(line_MR[1])*100)
+			if distance <= 500 :
+				dist.append(float(line_MR[0])) #Charge les données des mutations dans les variables associées
+				MR.append(float(line_MR[1])*100)
 
-	plt.plot(dist,MR)
+	plt.plot(dist,MR, color="black")
+	plt.axvline(0, color='red', linewidth=2, label='NIEBs borders')
+	plt.axvspan(-50, 0, zorder=1, alpha=0.1, color='#cc0000', label='Inside NIEBs')
+	plt.axvspan(0, 500, zorder=1, alpha=0.1, color='#00cccc', label='Inter NIEBs')
 	plt.title("Mutation rates around NIEBs")
 	plt.xlabel("Distance from NIEBs")
 	plt.ylabel("Mutation rate (%)")
