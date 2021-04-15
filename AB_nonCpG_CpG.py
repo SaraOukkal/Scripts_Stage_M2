6b0ba4605de71a2e5d65088a8be4f73da6ccf03a
SaraOukkal <sara.oukkal@ens-lyon.fr>
2021-04-15 09:48:46 +0200

    newCpG script

--- AB_nonCpG_CpG.py
+++ AB_nonCpG_CpG.py
@@ -48,33 +48,33 @@ def remove_CpG(chromosome, nonCpG, CpG):
 	Trie les bases ancestrales en CpG et nonCpG et les écrit dans deux fichiers
 	"""
 	
-		for i in range(len(chromosome)):
-			chrom=chromosome[i][0]
-			pos=int(chromosome[i][1])
-			nuc=chromosome[i][2]
-			EA=chromosome[i][3]
+	for i in range(len(chromosome)):
+		chrom=chromosome[i][0]
+		pos=int(chromosome[i][1])
+		nuc=chromosome[i][2]
+		EA=chromosome[i][3]
 			
-			if i == len(chromosome)-1: #Si c'est la fin du chromosome
-				break
-				
-			pos2=int(chromosome[i+1][1]) #Base qui suit dans le fichier 
-			EA2=chromosome[i+1][3]
+		if i == len(chromosome)-1: #Si c'est la fin du chromosome
+			break
 			
-			if EA == "C": #Si c'est un C
-				if pos2 == pos+1: #Si la base suivante du fichier la suit dans la séquence 
-					if EA2 == "G": #Si c'est un CpG 
-						#print("CpG")
-						CpG.write("{}\t{}\t{}\t{}\n".format(chrom, pos, nuc, EA)) 
-				
-					else: #Si ce n'est pas un CpG
-						nonCpG.write("{}\t{}\t{}\t{}\n".format(chrom, pos, nuc, EA)) 
+		pos2=int(chromosome[i+1][1]) #Base qui suit dans le fichier 
+		EA2=chromosome[i+1][3]
+		
+		if EA == "C": #Si c'est un C
+			if pos2 == pos+1: #Si la base suivante du fichier la suit dans la séquence 
+				if EA2 == "G": #Si c'est un CpG 
+					#print("CpG")
+					CpG.write("{}\t{}\t{}\t{}\n".format(chrom, pos, nuc, EA)) 
 			
-				else: #Si la base suivante de la séquence n'est pas une base ancestrale
-					continue #Passe directement à l'itération suivante
-						
-			else: #Si ce n'est pas un C 
-				nonCpG.write("{}\t{}\t{}\t{}\n".format(chrom, pos, nuc, EA)) 
-			
+				else: #Si ce n'est pas un CpG
+					nonCpG.write("{}\t{}\t{}\t{}\n".format(chrom, pos, nuc, EA)) 
+		
+			else: #Si la base suivante de la séquence n'est pas une base ancestrale
+				continue #Passe directement à l'itération suivante
+					
+		else: #Si ce n'est pas un C 
+			nonCpG.write("{}\t{}\t{}\t{}\n".format(chrom, pos, nuc, EA)) 
+		
 def main(): 
 	parser = argparse.ArgumentParser()
 	
