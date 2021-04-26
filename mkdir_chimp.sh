832f65f4915dc138c63115489017b8c5fbee1342
SaraOukkal <sara.oukkal@ens-lyon.fr>
2021-04-26 17:55:11 +0200

    chimp analysis

---
+++ mkdir_chimp.sh
@@ -0,0 +1,52 @@
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/
+
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/Left/
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/Left/CpG
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/Left/CpG/Plots
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/Left/nonCpG
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/Left/nonCpG/Plots
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/Left/All
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/Left/All/Plots
+
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/Right/
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/Right/CpG
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/Right/CpG/Plots
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/Right/nonCpG
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/Right/nonCpG/Plots
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/Right/All
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/Right/All/Plots
+
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/LR/
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/LR/CpG
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/LR/CpG/Plots
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/LR/nonCpG
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/LR/nonCpG/Plots
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/LR/All
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/All_inter_bar/LR/All/Plots
+
+
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/
+
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/Left/
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/Left/CpG
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/Left/CpG/Plots
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/Left/nonCpG
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/Left/nonCpG/Plots
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/Left/All
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/Left/All/Plots
+
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/Right/
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/Right/CpG
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/Right/CpG/Plots
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/Right/nonCpG
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/Right/nonCpG/Plots
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/Right/All
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/Right/All/Plots
+
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/LR/
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/LR/CpG
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/LR/CpG/Plots
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/LR/nonCpG
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/LR/nonCpG/Plots
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/LR/All
+mkdir /media/disk1/soukkal/StageM2/Stage_M1/Chimp/Ali_Results/1000_inter_bar/LR/All/Plots
