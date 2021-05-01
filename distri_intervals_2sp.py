3a7da1753e5888344cee5420c0638a10149eddd1
SaraOukkal <sara.oukkal@ens-lyon.fr>
2021-05-01 17:35:40 +0200

    distri intervals 2 sp

--- distri_intervals_2sp.py
+++ distri_intervals_2sp.py
@@ -41,8 +41,8 @@ def distri_inter_bar(intervals):
 			sizes.append(size)
 		
 	return sizes
-
-def plot (distribution1, distribution2, filename, output_dir):
+	
+def plot (distribution1, distribution2, output_dir):
 	
 	plt.figure(figsize=(10,10))	
 	plt.hist(distribution1, bins=20, color="limegreen", alpha=0.3, ec="darkgreen")
@@ -59,6 +59,61 @@ def plot (distribution1, distribution2, filename, output_dir):
 	plt.savefig(filepath)	
 	plt.clf()	
 	
+def inter_bar_1000(intervals1, intervals2, output_dir):	
+	f1=open(intervals1,"r")
+	inter1=f1.readlines()
+	f2=open(intervals2,"r")
+	inter2=f2.readlines()
+	
+	types=["less", "more"]
+	less1=0
+	more1=0
+	size1=[]
+	less2=0
+	more2=0
+	size2=[]
+	
+	for l in inter1:
+		line=l.strip().split("\t")
+		st=int(line[2])
+		end=int(line[3])	
+		sz=end-st
+		if sz < 1000:
+			less1+=1
+		else:
+			more1+=1
+			
+	size1.append(less1)
+	size1.append(more1)
+	
+	for l in inter2:
+		line=l.strip().split("\t")
+		st=int(line[2])
+		end=int(line[3])	
+		sz=end-st
+		if sz < 1000:
+			less2+=1
+		else:
+			more2+=1
+			
+	size2.append(less2)
+	size2.append(more2)
+	
+	plt.figure(figsize=(10,10))	
+	plt.bar(types,size1, color="limegreen", alpha=0.3, ec="darkgreen")
+	plt.bar(types,size2, color="dodgerblue", alpha=0.3, ec="darkblue")
+	plt.axes().minorticks_on()
+	plt.axes().tick_params(axis='both', which='major', direction='in', length= 8, width=2)
+	plt.axes().tick_params(axis='both', which='minor', direction='in', length= 4, width=1.5)
+	plt.xticks(fontsize=20)
+	plt.yticks(fontsize=20)
+	plt.title("Size of InterNIEBs", fontsize=20)
+	plt.ylabel("Number", fontsize=16)
+	filename="More_less.png"
+	filepath=os.path.join(output_dir, filename)
+	plt.savefig(filepath)	
+	plt.clf()
+
 				
 def main(): 
 	parser = argparse.ArgumentParser()
@@ -83,6 +138,8 @@ def main():
 	distribution3= distri_inter_bar(args.input_inter1)
 	distribution4= distri_inter_bar(args.input_inter2)
 	plot(distribution3, distribution4,"Distri_inter_NIEBs.png", args.output)
+	
+	inter_bar_1000(args.input_inter1, args.input_inter2, args.output)
 
 if "__main__" == __name__:
 	main()
